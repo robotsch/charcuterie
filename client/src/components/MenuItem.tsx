ef92b853c493620c0664913b46b29646da3aa959
otrachea <xtrachea@gmail.com>
2022-05-15 15:53:48 -0400

    Remove boilerplate, add barebone MenuItem component

---
+++ client/src/components/MenuItem.tsx
@@ -0,0 +1,14 @@
+export default function MenuItem() {
+  return (
+    <section className="menu-item">
+      <img alt="FOOD"></img>
+      <h1>Seaweed and tofu salad</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <div>
+        $1.50
+      </div>
+    </section>
+  )
+}
