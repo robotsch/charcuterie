32b7b63e6a07658227779e2d2a1e78c235a4a009
robotsh <christianhumble22@gmail.com>
2022-05-16 11:53:50 -0400

    add input sanitization check

---
+++ server/src/routes/table-session-router.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response, Router,} from 'express';
+
+const router: Router = express.Router()
+
+router.get("/", (req: Request, res: Response) => {
+
+  const regex = /^[0-9a-zA-Z]{1,16}$/
+
+  const id1 = req.query.id1 as string
+  const id2 = req.query.id as string
+  
+  if(id1.match(regex) && id2.match(regex)){
+    req.session.restaurant_id = id1
+    req.session.table_id = id2
+  } else {
+    res.send('Invalid parameters')
+  }
+
+})
+
+module.exports = router
