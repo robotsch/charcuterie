55972472666ebb80f85d5978b07f170a9564bc51
otrachea <xtrachea@gmail.com>
2022-05-16 12:30:39 -0400

    Use regular css for images in MenuItems

--- client/src/components/MenuItem.tsx
+++ client/src/components/MenuItem.tsx
@@ -1,6 +1,7 @@
-import { Typography } from "@mui/material";
+import { CardMedia, Typography } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import { CardActionArea } from "@mui/material";
 
 import { FoodItem } from "../../ts/foodItem_interface";
 
@@ -10,19 +11,26 @@ export default function MenuItem(props: FoodItem) {
   const { name, price, url, description } = props;
 
   return (
-    <Card sx={{ maxWidth: 900 }}>
-      <CardContent sx={{ padding: 0 }}>
-        <div className="menuItem">
-          <div className="menuItem-img-container">
-            <img src={url} alt="FOOD"></img>
+    <Card sx={{ maxWidth: 900, height: 120 }}>
+      <CardActionArea>
+        <CardContent sx={{ padding: 0}}>
+          {/* <CardMedia
+            component="img"
+            style={{ height: 100, width: 100 }}
+            image={url}
+          /> */}
+          <div className="menuItem">
+            <div className="menuItem-img-container">
+              <img src={url} alt="FOOD"></img>
+            </div>
+            <div>
+              <h5 className="mont">{name}</h5>
+              <Typography variant="body2">{description}</Typography>
+              <div>${(price / 100).toFixed(2)}</div>
+            </div>
           </div>
-          <div>
-            <h5 className="mont">{name}</h5>
-            <Typography>{description}</Typography>
-            <div>${(price / 100).toFixed(2)}</div>
-          </div>
-        </div>
-      </CardContent>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
