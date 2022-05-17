6fb816980103feafeada5dbd1fecc20574c49d2a
robotsh <christianhumble22@gmail.com>
2022-05-17 13:27:47 -0400

    add socket.io skeleton

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,26 @@
 require('dotenv').config({ silent: true });
 import express, { Request, Response } from 'express';
+import { createServer } from 'http'
 import cors from 'cors';
 import expressSession from 'express-session';
 import MongoStore from 'connect-mongo';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import QRcode from 'qrcode';
+import { Server } from 'socket.io'
 
+const clientPromise = require('./db/db')
 
 const app = express();
-
-const PORT = 3000;
-
 app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json());
+
+/**
+ * ============================================================ 
+ * Session setup
+ * ============================================================
+ */
 
 declare module 'express-session' {
   export interface SessionData {
@@ -23,8 +29,6 @@ declare module 'express-session' {
     name: string | undefined,
   }
 }
-
-const clientPromise = require('./db/db')
 
 app.use(
   expressSession({
@@ -38,6 +42,22 @@ app.use(
   })
 );
 
+/**
+ * ============================================================ 
+ * Socket setup
+ * ============================================================
+ */
+
+const server = createServer(app)
+const io = new Server(server, {
+
+})
+
+io.on('connection', (socket) => {
+
+})
+
+
 // Router imports
 const sessionRoute = require('./routes/table-session-router');
 const customerNameRoute = require('./routes/name-input-router');
@@ -50,4 +70,4 @@ app.get('/', (req: Request, res: Response) => {
   res.send('test');
 });
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+server.listen(3000, () => console.log(`Server running on ${3000}`));
