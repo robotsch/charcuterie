394e0f881370a94c407b8a8e0b6afac2d97e8adb
otrachea <xtrachea@gmail.com>
2022-05-18 10:02:49 -0400

    Connect to web socket after confirming name

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,13 +1,28 @@
 import { TextField, Button } from "@mui/material";
 import resolveProps from "@mui/utils/resolveProps";
 import axios from "axios";
+import { useRef, useEffect } from "react";
+import { io, Socket } from "socket.io-client";
 
 export default function Landing() {
+  axios.defaults.withCredentials = true;
+  const ws = useRef<null | any>(null);
+
+  useEffect(() => {}, []);
+
+  const join = () => {
+    ws.current = io("http://localhost:3001", { query: { data: "test" } });
+    ws.current.emit("join", { restaurant: 1, table: 1, name: "test" });
+    return () => {
+      ws.current.disconnect();
+    };
+  };
+
   const submitName = (event: any) => {
     event.preventDefault();
     const name = event.target[0].value;
     console.log("NAME", name);
-    axios.defaults.withCredentials = true;
+
     axios
       .get("http://localhost:3001/api/landing?id1=1&id2=2")
       .then((res) => {
@@ -40,7 +55,9 @@ export default function Landing() {
           variant="standard"
           placeholder="(max 15 characters)"
         ></TextField>
-        <Button type="submit">Confirm</Button>
+        <Button type="submit" onClick={join}>
+          Confirm
+        </Button>
       </form>
     </div>
   );
