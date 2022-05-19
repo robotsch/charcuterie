89a43d742375813579d9bc7aa239d7b035ea7ced
otrachea <xtrachea@gmail.com>
2022-05-19 11:27:46 -0400

    Hot fix

--- server/src/services/database.service.ts
+++ server/src/services/database.service.ts
@@ -1,39 +1,55 @@
 // External Dependencies
-import * as mongoDB from "mongodb";
-import * as dotenv from "dotenv";
+import * as mongoDB from 'mongodb';
+import * as dotenv from 'dotenv';
 
 // Global Variables
-export const collections: { 
-  restaurants?: mongoDB.Collection, 
-  tables?: mongoDB.Collection, 
-  users?: mongoDB.Collection, 
-
-} = {}
-
+export const collections: {
+  restaurants?: mongoDB.Collection;
+  tables?: mongoDB.Collection;
+  users?: mongoDB.Collection;
+} = {};
 
 // Initialize Connection
-export async function connectToDatabase () {
-   dotenv.config();
+export async function connectToDatabase() {
+  dotenv.config();
 
-   const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING ? undefined) ;
-           
-   await client.connect();
-       
-   const db: mongoDB.Db = client.db(process.env.DB_NAME);
-  
-   const restaurantsCollection: mongoDB.Collection = db.collection(process.env.RESTAURANTS_COLLECTION_NAME);
+  const DB_CONN_STRING: string = process.env.DB_CONN_STRING ?? 'null';
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(DB_CONN_STRING);
 
- collections.restaurants = restaurantsCollection;
+  await client.connect();
 
-    const tablesCollection: mongoDB.Collection = db.collection(process.env.TABLES_COLLECTION_NAME);
+  const db: mongoDB.Db = client.db(process.env.DB_NAME);
 
- collections.tables = tablesCollection;
+  const RESTAURANTS_COLLECTION_NAME: string =
+    process.env.RESTAURANTS_COLLECTION_NAME ?? 'null';
+  const restaurantsCollection: mongoDB.Collection = db.collection(
+    RESTAURANTS_COLLECTION_NAME
+  );
 
-    const usersCollection: mongoDB.Collection = db.collection(process.env.USERS_COLLECTION_NAME);
+  collections.restaurants = restaurantsCollection;
 
- collections.users = usersCollection;
-      
-        console.log(`Successfully connected to database: ${db.databaseName} and collection: ${restaurantsCollection.collectionName}`);
-        console.log(`Successfully connected to database: ${db.databaseName} and collection: ${tablesCollection.collectionName}`);
-        console.log(`Successfully connected to database: ${db.databaseName} and collection: ${usersCollection.collectionName}`);
+  const TABLES_COLLECTION_NAME: string =
+    process.env.TABLES_COLLECTION_NAME ?? 'null';
+  const tablesCollection: mongoDB.Collection = db.collection(
+    TABLES_COLLECTION_NAME
+  );
+
+  collections.tables = tablesCollection;
+  const USERS_COLLECTION_NAME: string =
+    process.env.USERS_COLLECTION_NAME ?? 'null';
+  const usersCollection: mongoDB.Collection = db.collection(
+    USERS_COLLECTION_NAME
+  );
+
+  collections.users = usersCollection;
+
+  console.log(
+    `Successfully connected to database: ${db.databaseName} and collection: ${restaurantsCollection.collectionName}`
+  );
+  console.log(
+    `Successfully connected to database: ${db.databaseName} and collection: ${tablesCollection.collectionName}`
+  );
+  console.log(
+    `Successfully connected to database: ${db.databaseName} and collection: ${usersCollection.collectionName}`
+  );
 }
