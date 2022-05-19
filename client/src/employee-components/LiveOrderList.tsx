fe6516eb09a08bfbf18e6b276f23e695481e7666
robotsh <christianhumble22@gmail.com>
2022-05-19 14:33:07 -0400

    exploratory socket testing

--- client/src/employee-components/LiveOrderList.tsx
+++ client/src/employee-components/LiveOrderList.tsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
 import LiveOrder from "./ListOrder";
-
 import { Typography } from "@mui/material";
-
 import { orderList } from "../mockdata";
+import ws from "../sockets/socket"
 
 export default function LiveOrderList() {
+  useEffect(() => {
+    ws.emit('EMPLOYEE', 'restaurant')
+    
+    ws.on('SUBMIT_ORDER', (order) => {
+      console.log(order)
+    })
+
+    return () => {
+      ws.off('test')
+    }
+  }, [])
+  
   return (
     <>
       <Typography variant="h4">Live Order Feed</Typography>
