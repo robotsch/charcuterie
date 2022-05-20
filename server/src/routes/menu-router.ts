479d63bf61f871dc50d330452e78b5d65f72310b
robotsh <christianhumble22@gmail.com>
2022-05-20 13:54:52 -0400

    add menu route

--- server/src/routes/menu-router.ts
+++ server/src/routes/menu-router.ts
@@ -1,23 +1,21 @@
 import express, { Request, Response, Router,} from 'express';
+import * as rQueries from '../db/queries/01_restaurants';
+import * as sanitize from 'mongo-sanitize'
+const ObjectId = require('mongodb').ObjectId;
 
 const router: Router = express.Router()
 
 router.get("/", (req: Request, res: Response) => {
-
-  return Promise.all([
-    // menuQueries.getAllMenuItems(),
-    // menuQueries.getAllCategories(),
-  ])
-  .then((data) => {
-    const mutatedData = ''
-
-    res.send(
-      JSON.stringify({mutatedData})
-    )
-  })
-  .catch((err) => {
-    res.status(500).send(`Failed to get menu items: ${err}`)
-  })
+  if(req.query.id) {
+    const restaurant = sanitize(req.query.id)
+    rQueries.getMenuByRestaurantId(ObjectId(restaurant))
+    .then((data) => {
+      res.send(data)
+    })
+    .catch((err) => {
+      res.status(500).send(`Failed to get menu items: ${err}`)
+    })
+  }
 })
 
 module.exports = router
