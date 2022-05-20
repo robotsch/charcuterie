43c3839206b4625cb4465e4979271b888453b7e1
robotsh <christianhumble22@gmail.com>
2022-05-20 15:24:34 -0400

    add order insertion functionality

--- server/src/routes/order-insert-router.ts
+++ server/src/routes/order-insert-router.ts
@@ -1,29 +1,44 @@
 import express, { Request, Response, Router } from 'express';
-// import * as rQueries from '../db/queries/01_restaurants';
-// import * as tQueries from '../db/queries/02_tables';
-import * as oQueries from '../db/queries/03_orders'
+import * as oQueries from '../db/queries/03_orders';
 
 const ObjectId = require('mongodb').ObjectId;
-const sanitize = require('mongo-sanitize')
+const sanitize = require('mongo-sanitize');
 
 const router: Router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  // const restaurant = sanitize(req.body.restaurant)
-  // const table = sanitize(req.body.table)
-  // const order = sanitize(req.body.order)
-  const table = "6283f6a703f54b7c82c5fffc"
-  const custArr = {
-    name: 'Christian', 
-    sub_orders: [
-      { menu_item_id: ObjectId('6283f1d9804b848eb5e45600'), quantity: 2}
-    ]
+router.post('/', (req: Request, res: Response) => {
+  const restaurant = sanitize(req.body.restaurant)
+  const table = sanitize(req.body.table)
+  const orders = sanitize(req.body.order)
+
+  const custArr = []
+
+  for(const name in orders) {
+    const sub_orders: any[] = []
+    
+    orders[name].order.forEach((item: any) => {
+      sub_orders.push({
+        menu_item_id: ObjectId(item.id),
+        quantity: item.quantity
+      })
+    })
+
+    const orderObj = {
+      name: name,
+      sub_orders: sub_orders
+    }
+    
+    custArr.push(orderObj)
   }
   
-  // oQueries.createOrderByTableId(ObjectId(table), custArr)
-  //   .then((data) => {
-  //     res.send(data)
-  //   })
+  oQueries.createOrderByTableId(ObjectId(table), custArr)
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      res.status(500).send(`Failed to submit order: ${err}`)
+    })
+
 });
 
 module.exports = router;
