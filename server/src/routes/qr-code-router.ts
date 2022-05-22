fa05365794b549153d6fa55d6964940b432f0d12
robotsh <christianhumble22@gmail.com>
2022-05-22 19:30:11 -0400

    remove hardcoded localhost generation

--- server/src/routes/qr-code-router.ts
+++ server/src/routes/qr-code-router.ts
@@ -7,7 +7,7 @@ const router: Router = express.Router()
 router.post("/", apiAuthCheck, (req: Request, res: Response) => {
   const [restaurant, table] = req.body
   
-  const QR_CODE = qrcode.toDataURL(`http://localhost:3000/landing?id1=${restaurant}&id2=${table}`, (err, url) => {
+  qrcode.toDataURL(`${process.env.CLIENT_ORIGIN}/landing?id1=${restaurant}&id2=${table}`, (err, url) => {
     res.send(`<img src=${url}></img>`)
   })
 })
