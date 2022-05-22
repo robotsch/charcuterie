f19a75004f2710c76087e626d1f6a232b0ee2e82
robotsh <christianhumble22@gmail.com>
2022-05-22 18:41:47 -0400

    implement readable names router

--- server/src/routes/readable-names-router.ts
+++ server/src/routes/readable-names-router.ts
@@ -1,29 +1,27 @@
-import express, { Request, Response, Router,} from 'express';
+import express, { Request, Response, Router } from 'express';
 import * as rQueries from '../db/queries/01_restaurants';
-import * as tQueries from '../db/queries/02_tables'
+import * as tQueries from '../db/queries/02_tables';
 
 const sanitize = require('mongo-sanitize');
 const ObjectId = require('mongodb').ObjectId;
 
-const router: Router = express.Router()
+const router: Router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  
-  // if(req.query.restaurant && req.query.table) {
-  //   const restaurant = sanitize(req.query.restaurant)
-  //   const table = sanitize(req.query.table)
+router.get('/', (req: Request, res: Response) => {
+  if (req.query.restaurant && req.query.table) {
+    const restaurant = sanitize(req.query.restaurant);
+    const table = sanitize(req.query.table);
 
-  const restaurant = "6283f1d9804b848eb5e4560c"
-  const table = "6283f6a703f54b7c82c5fffd"
-  //   rQueries.getRestaurantsWithId
-  // }
-  // rQueries.getAllRestaurants()
-  //   .then((data) => {
-  //     console.log(data)
-  //   })
-  
-  res.send('done')
+    Promise.all([
+      rQueries.getRestaurantsWithId(ObjectId(restaurant)),
+      tQueries.getTableReadableIdById(ObjectId(table)),
+    ]).then((data) => {
+      const names = { restaurant: data[0].name, table: data[1] };
+      res.send(names);
+    });
+  } else {
+    res.send('Invalid request')
+  }
+});
 
-})
-
-module.exports = router
+module.exports = router;
