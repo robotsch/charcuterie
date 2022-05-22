41d0204c61ed07b89d9d971e76eff2fd6118d594
robotsh <christianhumble22@gmail.com>
2022-05-22 18:53:16 -0400

    remove extra console log

--- server/src/routes/readable-names-router.ts
+++ server/src/routes/readable-names-router.ts
@@ -17,7 +17,6 @@ router.get('/', (req: Request, res: Response) => {
       tQueries.getTableReadableIdById(ObjectId(table)),
     ]).then((data) => {
       const names = { restaurant: data[0].name, table: data[1] };
-      console.log(names)
       res.send(names);
     });
   } else {
