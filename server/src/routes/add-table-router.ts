84ed36eb5782fd324933baf949893ad0c1d63d02
robotsh <christianhumble22@gmail.com>
2022-05-23 19:22:47 -0400

    implement table routes

---
+++ server/src/routes/add-table-router.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response, Router } from 'express';
+import { apiAuthCheck } from '../middleware/auth-redirects';
+import * as rQueries from '../db/queries/01_restaurants';
+
+const sanitize = require('mongo-sanitize');
+const ObjectId = require('mongodb').ObjectId;
+
+const router: Router = express.Router();
+
+router.post('/', apiAuthCheck ,(req: Request, res: Response) => {
+  const data = {
+    price: sanitize(req.body.price),
+    name: sanitize(req.body.name),
+    description: sanitize(req.body.description),
+    image_url: sanitize(req.body.image_url),
+    category: sanitize(req.body.category),
+  };
+  const restaurant = sanitize(req.session.restaurant_id)
+
+  rQueries
+    .addMenuItemByRestaurantId(ObjectId(restaurant), data)
+    .then((res) => {
+      res.send(res);
+    });
+});
+
+module.exports = router;
