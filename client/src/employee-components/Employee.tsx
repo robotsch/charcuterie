bd52f7e30cc94e252469e72cabd1eefa622a89fd
Elroy Hui <huielroy@hotmail.com>
2022-05-23 10:52:04 -0400

    to pull

--- client/src/employee-components/Employee.tsx
+++ client/src/employee-components/Employee.tsx
@@ -2,28 +2,28 @@ import LiveOrderList from "./LiveOrderList";
 import SideBar from "./SideBar";
 import Box from "@mui/material/Box";
 import TablesStatus from "./TablesStatus";
-import axios from 'axios'
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Employee() {
-
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log('running')
-    axios.get('http://localhost:3001/api/session', {withCredentials: true})
+    console.log("running");
+    axios
+      .get("http://localhost:3001/api/session", { withCredentials: true })
       .then((data) => {
-        if(data.data.isLoggedIn) {
-          setLoading(false)
+        if (data.data.isLoggedIn) {
+          setLoading(false);
         } else {
-          alert('Alerted')
+          alert("Alerted");
         }
       })
-      .catch((err) => console.log(err))
-  }, [])
+      .catch((err) => console.log(err));
+  }, []);
 
-  if(loading){
-    return <div>Loading</div>
+  if (loading) {
+    return <div>Loading</div>;
   }
 
   return (
