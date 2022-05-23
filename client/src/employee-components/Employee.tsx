886b426d04d2a7f09a081104c044a82e73bafc36
robotsh <christianhumble22@gmail.com>
2022-05-22 21:58:14 -0400

    add employee auth check

--- client/src/employee-components/Employee.tsx
+++ client/src/employee-components/Employee.tsx
@@ -2,39 +2,39 @@ import LiveOrderList from "./LiveOrderList";
 import SideBar from "./SideBar";
 import Box from "@mui/material/Box";
 import TablesStatus from "./TablesStatus";
-import axios from 'axios'
+import axios from "axios";
 import { useEffect, useState } from "react";
+import EmployeeLogin from "./EmployeeLogin";
 
 export default function Employee() {
-
-  const [loading, setLoading] = useState(true)
+  const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    
-    const origin = '/api/session'
-    // const origin = 'http://localhost:3001/asdf'
+    // const origin = '/api/session'
+    const origin = "http://localhost:3001/api/session";
 
-    axios.get(origin, {withCredentials: true})
+    axios
+      .get(origin, { withCredentials: true })
       .then((data) => {
-        if(data.data.isLoggedIn) {
-          setLoading(false)
-        } else {
-          alert('Alerted')
+        if (data.data.isLoggedIn) {
+          setLoggedIn(true);
         }
       })
-      .catch((err) => console.log(err))
-  }, [])
-
-  if(loading){
-    return <div>Loading</div>
-  }
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <SideBar>
-      <Box component="main" sx={{ bgcolor: "background.default", p: 3 }}>
-        <TablesStatus />
-      </Box>
-      <LiveOrderList />
-    </SideBar>
+    <div>
+      {loggedIn ? (
+        <SideBar>
+          <Box component="main" sx={{ bgcolor: "background.default", p: 3 }}>
+            <TablesStatus />
+          </Box>
+          <LiveOrderList />
+        </SideBar>
+      ) : (
+        <EmployeeLogin />
+      )}
+    </div>
   );
 }
