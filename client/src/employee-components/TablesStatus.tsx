2c6982b0729fed5a06fe37767c226f603540db43
robotsh <christianhumble22@gmail.com>
2022-05-24 17:32:56 -0400

    pre-deploy fix

--- client/src/employee-components/TablesStatus.tsx
+++ client/src/employee-components/TablesStatus.tsx
@@ -36,7 +36,7 @@ export default function TablesStatus() {
     // )
     axios
       .get(
-        `http://localhost:3001/api/get-orders-restaurant?id=6283f1d9804b848eb5e4560c`
+        `/api/get-orders-restaurant?id=6283f1d9804b848eb5e4560c`
       )
       .then((res) => {
         setOrders(res.data);
