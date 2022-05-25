f1c1366ef9258d2393a160543817d597f9781d60
robotsh <christianhumble22@gmail.com>
2022-05-25 15:50:10 -0400

    formatting

--- server/src/db/seeds/01a_restaurants_drop.js
+++ server/src/db/seeds/01a_restaurants_drop.js
@@ -1,12 +1,12 @@
-let MongoClient = require("mongodb").MongoClient;
-let url = "mongodb://localhost:27017/";
+let MongoClient = require('mongodb').MongoClient;
+let url = 'mongodb://localhost:27017/';
 
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
-  let dbo = db.db("mydb");
-  dbo.collection("restaurants").drop(function (err, delOK) {
+  let dbo = db.db('mydb');
+  dbo.collection('restaurants').drop(function (err, delOK) {
     if (err) throw err;
-    if (delOK) console.log("Restaurants Collection purged");
+    if (delOK) console.log('Restaurants Collection purged');
     db.close();
   });
 });
