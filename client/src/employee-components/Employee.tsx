768f99ce1f8e2b7f9090af8ee2bf75009ce8681c
robotsh <christianhumble22@gmail.com>
2022-05-25 17:27:29 -0400

    add footer

--- client/src/employee-components/Employee.tsx
+++ client/src/employee-components/Employee.tsx
@@ -7,6 +7,10 @@ import EmployeeLogin from "./EmployeeLogin";
 import { CircularProgress } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import IconButton from "@mui/material/IconButton";
+
 import LiveOrderList from "./LiveOrderList";
 import PastOrders from "./PastOrders";
 import EmployeeMenuItems from "./EmployeeMenuItems";
@@ -43,7 +47,6 @@ export default function Employee() {
   }, []);
 
   return (
-    // <ThemeProvider theme={theme}>
     <div>
       {status === "loading" ? (
         <Box
@@ -65,17 +68,85 @@ export default function Employee() {
               p: 3,
               width: "100%",
               display: "flex",
-              justifyContent: "center",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
             {page === "HOME" && <TablesStatus />}
             {page === "ORDER_HISTORY" && <PastOrders />}
             {page === "MENU" && <EmployeeMenuItems />}
+            <Box
+              sx={{
+                display: "flex",
+                width: "60%",
+                height: "5em",
+                justifyContent: "space-around",
+                bgcolor: "white",
+                position: "fixed",
+                bottom: 0,
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "justify-content": "center",
+                  "align-items": "center",
+                }}
+              >
+                <span className="mont">Christian</span>
+                <div>
+                  <IconButton href="https://github.com/robotsch/">
+                    <GitHubIcon />
+                  </IconButton>
+                  <IconButton href="https://www.linkedin.com/in/christian-humble-bb1949163/">
+                    <LinkedInIcon />
+                  </IconButton>
+                </div>
+              </Box>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "justify-content": "center",
+                  "align-items": "center",
+                }}
+              >
+                <span className="mont">Francesca</span>
+                <div>
+                  <IconButton href="https://github.com/otrachea/">
+                    <GitHubIcon />
+                  </IconButton>
+                  <IconButton href="https://www.linkedin.com/in/francesca-h-111714174/">
+                    <LinkedInIcon />
+                  </IconButton>
+                </div>
+              </Box>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "justify-content": "center",
+                  "align-items": "center",
+                }}
+              >
+                <span className="mont">Elroy</span>
+                <div>
+                  <IconButton href="https://github.com/letsfighting/">
+                    <GitHubIcon />
+                  </IconButton>
+                  <IconButton href="https://www.linkedin.com/in/elroy-hui-991853bb/">
+                    <LinkedInIcon />
+                  </IconButton>
+                </div>
+              </Box>
+            </Box>
           </Box>
           <LiveOrderList />
         </SideBar>
       )}
     </div>
-    // </ThemeProvider>
   );
 }
